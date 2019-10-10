59e7e7e03ef0d3369cce990a03246f7df409f62b
serius1305 <hanomanhehe1@gmail.com>
2019-10-10 13:23:55 +0700

    Tugas E-LEARNING MVC BAGIAN 1-4

---
+++ app/controllers/about.php
@@ -0,0 +1,13 @@
+<?php 
+
+ class About {
+ 	public function index($nama = 'Muhammad Reggy Nugraha',$pekerjaan = 'Mahasiswa', $umur = 20)
+ 	{
+ 		echo "Halo,nama saya $nama, saya adalah seorang $pekerjaan . saya berumur $umur tahun.";
+ 	}
+ 	public function page(){
+
+ 	 }
+ 	 echo 'About/page';
+ 	}
+ }
